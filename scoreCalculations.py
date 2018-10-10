40b6d86e170b58b5c12e0e6a85e09262448fe134
Reda Hamioui <reda.hamioui@gmail.com>
2018-10-11 01:58:00 +0200

    Update scoreCalculations.py
    Add Tests

--- scoreCalculations.py
+++ scoreCalculations.py
@@ -1,16 +1,25 @@
 import unittest
-from testScore import *
+import mycode as m
+# from mycode import *
 
 class MyFirstTests(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(m.hello_world(), 'hello world')
 
-    def test_ScoreCalculations(self):
-        self.assertEqual(ScoreCal('Joseph', 15), '66%')
+    def test_score_joseph(self):
+        self.assertEqual(m.calculerScore("Joseph", 15), '66%')
 
-    #def ScoreCalculations(self):
-        #self.assertEqual(scoreCal(name, age), '50%')
+    def test_score_marie(self):
+        self.assertEqual(m.calculerScore("Marie", 33), '50%')
 
-    #def ScoreCalculations(self):
-        #self.assertEqual(scoreCal(name,age), '43%')
+    def test_score_marc(self):
+        self.assertEqual(m.calculerScore("Marc", 60), '43%')
 
-    #def ScoreCalculations(self):
-        #self.assertEqual(scoreCal(name,age), '75%')
+    def test_score_ely(self):
+        self.assertEqual(m.calculerScore("Ely", 28), '75%')
+
+
+if __name__ == '__main__':
+    unittest.main()
+    # test = MyFirstTests()
+    # test.test_hello()
